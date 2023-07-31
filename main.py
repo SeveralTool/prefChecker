240d6d29b5b36126dad82dc50685e8f0ba11197b
Nahuel Galeano <40505451+SeveralTool@users.noreply.github.com>
2023-07-31 19:33:49 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import json
+import re
 from tkinter import *
 
 # Cargar la base de datos JSON
+# Para probar, se debe modificar la ruta de db.json dependiendo de su estructura...
 with open('C:/Users/Nahue/OneDrive/Escritorio/PROGRAMACION/PYTHON/Cilan/test/db.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
 windows = Tk()
 windows.title("Consultor de Prefijos")
 
-# Label
-text_1 = Label(windows, text="Inserte el número de teléfono...")
+# Etiqueta
+text_1 = Label(windows, text="Inserte el numero de teléfono...")
 text_1.grid(row=0, column=0, columnspan=50, padx=50, pady=5)
 
-# Input
+# Campo de entrada
 input = Entry(windows, width=70)
 input.grid(row=1, column=0, columnspan=50, padx=50, pady=5)
 
@@ -20,27 +22,31 @@ def consultar_precio():
     numero_telefonico = input.get()
     prefijo = numero_telefonico[:3]
 
-    # Consultar el precio en la "base de datos"
-    if prefijo in data['prefijos']:
-        precio = data['prefijos'][prefijo]['precio']
-        pais = data['prefijos'][prefijo]['pais']
-        ciudad = data['prefijos'][prefijo]['ciudad']
-        # result.config(text={prefijo})
-        result.config(text=f'El prefijo: {prefijo}, es de {ciudad}, {pais}con un costo de ${precio} por minuto')
+    if not prefijo.isdigit():
+        result.config(text="Ingrese un prefijo válido (solo dígitos)")
     else:
-        result.config(text='El prefijo no está registrado en la base de datos')
+        # Consultar el precio en la "base de datos"
+        if prefijo in data['prefijos']:
+            precio = data['prefijos'][prefijo]['precio']
+            pais = data['prefijos'][prefijo]['pais']
+            ciudad = data['prefijos'][prefijo]['ciudad']
+            result.config(text=f'El prefjo: {prefijo}, es de {ciudad}, {pais} con un costo de ${precio} por minuto')
+        else:
+            result.config(text='El prefijo no está registrado en la base de datos')
 
 def del_input():
     input.delete(0, END)
+    result.config(text="")
 
-# Boton
+# Botón
 btn_Set = Button(windows, text='Consultar', width=15, height=1, command=consultar_precio)
 btn_Set.grid(row=3, columnspan=50, padx=2, pady=2)
-# Boton para borrar input
+
+# Botón para borrar el campo de entrada
 btn_Del = Button(windows, text='Borrar', width=15, height=1, command=del_input)
 btn_Del.grid(row=4, columnspan=50, padx=2, pady=2)
 
-# Label resultado
+# Etiqueta para el resultado
 result = Label(windows, text="", fg='red')
 result.grid(row=2, columnspan=50)
 
